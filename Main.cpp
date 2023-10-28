24d4535735f15c54f9ce18a6854b5d5c24dd40d0
zlaty12 <zlatimirbg2@gmail.com>
2023-10-28 13:06:57 +0100

    Textures

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
-
+#include<stb/stb_image.h>
+#include"Texture.h"
 #include"shaderClass.h"
 #include"VAO.h"
 #include"VBO.h"
@@ -12,21 +13,20 @@
 // Vertices 
 GLfloat vertices[] =
 {
-	//      Cordinets                            Color
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,	0.8f, 0.3f, 0.02f,	 // Lower left corner
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,		1.0f, 0.3f, 0.25f,	 // Lower right corner
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,	0.6f, 0.7f, 0.3f,	// Upper corner
-    -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,	 0.5f, 0.3f, 0.3f,	// Inner left
-	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,	1.0f , 0.7f, 0.5f,	 // Inner right
-	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f,		0.6f, 0.4f, 0.1f	 // Inner down
+	//      Cordinets                /  Color           
+	-0.5f, -0.5f, 0.0f,			    	0.8f, 0.3f, 0.02f,  0.0f, 0.0f,	 // Lower left corner
+	-0.5f, 0.5f, 0.0f,					1.0f, 0.3f, 0.25f,	0.0f,1.0f,     // Lower right corner
+	 0.5f, 0.5f,0.0f,			        0.6f, 0.7f, 0.3f,	1.0f,1.0f,     // Upper corner
+     0.5f, -0.5f,0.0f,	                0.5f, 0.3f, 0.3f,	1.0f, 0.0f     // Inner left
+	
 };
 
 // Indices for vertices order
 GLuint indices[] =
 {
-	0, 3, 5, // Lower left triangle
-	3, 2, 4, // Lower right triangle
-	5, 4, 1 // Upper triangle
+	0, 2, 1, // Lower left triangle
+	0, 3, 2, // Lower right triangle
+	
 };
 
 
@@ -76,14 +76,22 @@ int main()
 	EBO EBO1(indices, sizeof(indices));
 
 	// Links VBO atributes to VAO
-	VAO1.LinkAttrib(VBO1, 0,3, GL_FLOAT,6 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3* sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 0,3, GL_FLOAT,8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3* sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
+	// Texture
+	Texture Pirate("Pirate.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_UNSIGNED_BYTE);
+	Pirate.texUnit(shaderProgram, "tex0", 0);
 
+
+	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+	shaderProgram.Activate();
+	glUniform1i(tex0Uni, 0);
 
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
@@ -94,10 +102,12 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 		// Tell OpenGL what shader program to use
 		shaderProgram.Activate();
+		// Bind texture
+		Pirate.Bind();
 		// Bind the VAO 
 		VAO1.Bind();
 		// Draw primitives, number of indices, datatype of indices, index of indices
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
 		// GLFW events
@@ -111,7 +121,7 @@ int main()
 	VBO1.Delete();
 	EBO1.Delete();
 	shaderProgram.Delete();
-	// Delete window 
+	Pirate.Delete();
 	glfwDestroyWindow(window);
 	// Terminate GLFW 
 	glfwTerminate();
--- default.vert
+++ default.vert
@@ -2,12 +2,16 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 
+layout (location = 2) in vec2 aTex;
+
 // outputs a vec3 color to the fragment shader
 out vec3 color;
 
+out vec2 texCoord;
 void main()
 {
    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
    // assigns the color to the Vertex Data 
    color = aColor;
+   texCoord = aTex;
 }
